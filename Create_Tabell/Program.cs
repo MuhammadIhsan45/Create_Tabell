3b184bf69a7b2c092e6cfe983be0b9135c85cf72
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-03-16 15:20:07 +0700

    Create tabel Baru

--- Create_Tabell/Program.cs
+++ Create_Tabell/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Create_Tabell
 {
@@ -10,6 +11,31 @@ namespace Create_Tabell
     {
         static void Main(string[] args)
         {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=IHSAN\\MUHAMMADIHSAN;database=ProdiTI;User ID=sa;Password=123");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Cobaa (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel suksess dibuat");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
